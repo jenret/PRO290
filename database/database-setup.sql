01e9819774a0b2391e71863037a213ee1140b565
Xander Endre <45239432+ShadyAlexCodes@users.noreply.github.com>
2023-05-24 00:54:10 -0600

    Cleaned up Database

--- database/database-setup.sql
+++ database/database-setup.sql
@@ -1,185 +1,110 @@
--- Create databases
-CREATE DATABASE UserServiceDB;
-CREATE DATABASE RoleServiceDB;
-CREATE DATABASE CustomerServiceDB;
-CREATE DATABASE AddressServiceDB;
-CREATE DATABASE ContactInformationServiceDB;
-CREATE DATABASE OrderServiceDB;
-CREATE DATABASE ItemServiceDB;
-CREATE DATABASE InventoryServiceDB;
-
--- Use UserServiceDB
-USE UserServiceDB;
--- Create tables for UserService
-CREATE TABLE users (
-    id UNIQUEIDENTIFIER PRIMARY KEY,
-    first_name NVARCHAR(64) NOT NULL,
-    last_name NVARCHAR(64) NOT NULL,
-    email_address NVARCHAR(256) NOT NULL UNIQUE,
-    phone_number NVARCHAR(64) NOT NULL,
-    password NVARCHAR(256) NOT NULL,
-    is_verified BIT NOT NULL,
-    is_active BIT NOT NULL,
-    date_created DATETIME DEFAULT CURRENT_TIMESTAMP,
-    date_modified DATETIME DEFAULT CURRENT_TIMESTAMP
+use master;
+--  drop the database
+drop database if exists order_management_system;
+-- create the database
+create database order_management_system;
+-- use the database
+use order_management_system;
+-- user tables
+create table users (
+    id UNIQUEIDENTIFIER DEFAULT NEWID() PRIMARY KEY,
+    first_name nvarchar(64) not null,
+    last_name nvarchar(64) not null,
+    email_address nvarchar(256) not null,
+    phone_number nvarchar(64) not null,
+    password nvarchar(256) not null,
+    date_created datetime default current_timestamp,
+    date_modified datetime default current_timestamp
 );
--- Use RoleServiceDB
-USE RoleServiceDB;
--- Create tables for RoleService
-CREATE TABLE roles (
-    id UNIQUEIDENTIFIER PRIMARY KEY,
-    role_name NVARCHAR(64) NOT NULL,
-    date_created DATETIME DEFAULT CURRENT_TIMESTAMP,
-    date_modified DATETIME DEFAULT CURRENT_TIMESTAMP
+--  customers
+create table addresses (
+    id UNIQUEIDENTIFIER DEFAULT NEWID() PRIMARY KEY,
+    street_address nvarchar(128) not null,
+    street_address_two nvarchar(128) null,
+    city nvarchar(128) not null,
+    state nvarchar(128) not null,
+    zipcode nvarchar(15) not null,
+    country nvarchar(64) not null,
+    date_created datetime default current_timestamp,
+    date_modified datetime default current_timestamp
 );
-CREATE TABLE user_roles (
-    id UNIQUEIDENTIFIER PRIMARY KEY,
-    user_id UNIQUEIDENTIFIER NOT NULL,
-    role_id UNIQUEIDENTIFIER NOT NULL,
-    date_created DATETIME DEFAULT CURRENT_TIMESTAMP,
-    date_modified DATETIME DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE,
-    FOREIGN KEY (role_id) REFERENCES roles (id) ON DELETE CASCADE
+create table contact_information (
+    id UNIQUEIDENTIFIER DEFAULT NEWID() PRIMARY KEY,
+    first_name nvarchar(64) not null,
+    last_name nvarchar(64) not null,
+    phone_number nvarchar(64) not null,
+    email_address nvarchar(256) not null,
+    date_created datetime default current_timestamp,
+    date_modified datetime default current_timestamp
 );
-CREATE INDEX user_id_index ON user_roles(user_id);
-CREATE UNIQUE INDEX user_role_index ON user_roles(user_id, role_id);
--- Use CustomerServiceDB
-USE CustomerServiceDB;
--- Create tables for CustomerService
-CREATE TABLE addresses (
-    id UNIQUEIDENTIFIER PRIMARY KEY,
-    street_address NVARCHAR(128) NOT NULL,
-    street_address_two NVARCHAR(128) NULL,
-    city NVARCHAR(128) NOT NULL,
-    state NVARCHAR(128) NOT NULL,
-    zipcode NVARCHAR(8) NOT NULL,
-    country NVARCHAR(64) NOT NULL,
-    date_created DATETIME DEFAULT CURRENT_TIMESTAMP,
-    date_modified DATETIME DEFAULT CURRENT_TIMESTAMP
+create table customer (
+    id UNIQUEIDENTIFIER DEFAULT NEWID() PRIMARY KEY,
+    name nvarchar(64) not null,
+    contact_id uniqueidentifier not null,
+    address_id uniqueidentifier not null,
+    date_created datetime default current_timestamp,
+    date_modified datetime default current_timestamp,
+    foreign key (contact_id) references contact_information (id) on delete cascade,
+    foreign key (address_id) references addresses (id),
 );
-CREATE TABLE contact_information (
-    id UNIQUEIDENTIFIER PRIMARY KEY,
-    first_name NVARCHAR(64) NOT NULL,
-    last_name NVARCHAR(64) NOT NULL,
-    phone_number NVARCHAR(64) NOT NULL,
-    email_address NVARCHAR(256) NOT NULL UNIQUE,
-    is_active BIT NOT NULL,
-    date_created DATETIME DEFAULT CURRENT_TIMESTAMP,
-    date_modified DATETIME DEFAULT CURRENT_TIMESTAMP
+create index customer_id_index on customer(id);
+--  order management system
+create table vendor (
+    id UNIQUEIDENTIFIER DEFAULT NEWID() PRIMARY KEY,
+    name nvarchar(64) not null,
+    contact_id uniqueidentifier not null,
+    address_id uniqueidentifier not null,
+    date_created datetime default current_timestamp,
+    date_modified datetime default current_timestamp,
 );
-CREATE TABLE customer_parent (
-    id UNIQUEIDENTIFIER PRIMARY KEY,
-    name NVARCHAR(64) NOT NULL,
-    address_id UNIQUEIDENTIFIER NOT NULL,
-    contact_id UNIQUEIDENTIFIER NOT NULL,
-    is_active BIT NOT NULL,
-    date_created DATETIME DEFAULT CURRENT_TIMESTAMP,
-    date_modified DATETIME DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (address_id) REFERENCES addresses (id) ON DELETE CASCADE,
-    FOREIGN KEY (contact_id) REFERENCES contact_information (id) ON DELETE CASCADE
+create table items (
+    id UNIQUEIDENTIFIER DEFAULT NEWID() PRIMARY KEY,
+    name nvarchar(64) not null,
+    description nvarchar(128) not null,
+    vendor_id uniqueidentifier not null,
+    foreign key (vendor_id) references vendor (id) on delete cascade,
 );
-CREATE INDEX customer_parent_id_index ON customer_parent(id);
-CREATE TABLE customer (
-    id UNIQUEIDENTIFIER PRIMARY KEY,
-    name NVARCHAR(64) NOT NULL,
-    parent_id UNIQUEIDENTIFIER NOT NULL,
-    address_id UNIQUEIDENTIFIER NOT NULL,
-    shipping_address_id UNIQUEIDENTIFIER NOT NULL,
-    primary_contact_id UNIQUEIDENTIFIER NOT NULL,
-    is_active BIT NOT NULL,
-    date_created DATETIME DEFAULT CURRENT_TIMESTAMP,
-    date_modified DATETIME DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (parent_id) REFERENCES contact_information (id) ON DELETE CASCADE,
-    FOREIGN KEY (address_id) REFERENCES addresses (id),
-    FOREIGN KEY (shipping_address_id) REFERENCES addresses (id),
-    FOREIGN KEY (primary_contact_id) REFERENCES contact_information (id)
+create index items_id_index on items(id);
+create table bill_of_materials (
+    id UNIQUEIDENTIFIER DEFAULT NEWID() PRIMARY KEY,
+    parent_item_id uniqueidentifier not null,
+    child_item_id uniqueidentifier not null,
+    quantity int not null,
+    date_created datetime default current_timestamp,
+    date_modified datetime default current_timestamp,
+    foreign key (parent_item_id) references items (id),
+    foreign key (child_item_id) references items (id),
 );
-CREATE INDEX customer_id_index ON customer(id);
-CREATE TABLE customer_contacts (
-    id UNIQUEIDENTIFIER PRIMARY KEY,
-    customer_id UNIQUEIDENTIFIER NOT NULL,
-    contact_id UNIQUEIDENTIFIER NOT NULL,
-    FOREIGN KEY (customer_id) REFERENCES customer (id) ON DELETE CASCADE,
-    FOREIGN KEY (contact_id) REFERENCES contact_information (id)
+create index parent_bill_of_materials_index on bill_of_materials(parent_item_id);
+create index child_bill_of_materials_index on bill_of_materials(child_item_id);
+create table orders (
+    id UNIQUEIDENTIFIER DEFAULT NEWID() PRIMARY KEY,
+    customer_id uniqueidentifier not null,
+    order_date datetime not null,
+    shipping_date datetime not null,
+    notes nvarchar(256) null,
+    order_status_id bit not null,
+    date_created datetime default current_timestamp,
+    date_modified datetime default current_timestamp,
+    foreign key (customer_id) references customer (id) on delete cascade
 );
--- Use OrderServiceDB
-USE OrderServiceDB;
--- Create tables for OrderService
-CREATE TABLE vendor (
-    id UNIQUEIDENTIFIER PRIMARY KEY,
-    name NVARCHAR(64) NOT NULL,
-    date_created DATETIME DEFAULT CURRENT_TIMESTAMP,
-    date_modified DATETIME DEFAULT CURRENT_TIMESTAMP
+create table order_items (
+    id UNIQUEIDENTIFIER DEFAULT NEWID() PRIMARY KEY,
+    order_id uniqueidentifier not null,
+    item_id uniqueidentifier not null,
+    quantity int not null,
+    price decimal(10, 2) not null,
+    notes nvarchar(256) null,
+    date_created datetime default current_timestamp,
+    date_modified datetime default current_timestamp,
+    foreign key (order_id) references orders (id) on delete cascade,
+    foreign key (item_id) references items (id)
 );
-CREATE TABLE vendor_contacts (
-    id UNIQUEIDENTIFIER PRIMARY KEY,
-    vendor_id UNIQUEIDENTIFIER NOT NULL,
-    contact_id UNIQUEIDENTIFIER NOT NULL,
-    FOREIGN KEY (vendor_id) REFERENCES vendor (id) ON DELETE CASCADE,
-    FOREIGN KEY (contact_id) REFERENCES contact_information (id)
-);
-CREATE TABLE vendor_addresses (
-    id UNIQUEIDENTIFIER PRIMARY KEY,
-    vendor_id UNIQUEIDENTIFIER NOT NULL,
-    address_id UNIQUEIDENTIFIER NOT NULL,
-    FOREIGN KEY (vendor_id) REFERENCES vendor (id) ON DELETE CASCADE,
-    FOREIGN KEY (address_id) REFERENCES addresses (id)
-);
-CREATE TABLE items (
-    id UNIQUEIDENTIFIER PRIMARY KEY,
-    name NVARCHAR(64) NOT NULL,
-    description NVARCHAR(128) NOT NULL,
-    vendor_id UNIQUEIDENTIFIER NOT NULL,
-    FOREIGN KEY (vendor_id) REFERENCES vendor (id) ON DELETE CASCADE
-);
-CREATE INDEX items_id_index ON items(id);
-CREATE TABLE bill_of_materials (
-    id UNIQUEIDENTIFIER PRIMARY KEY,
-    parent_item_id UNIQUEIDENTIFIER NOT NULL,
-    child_item_id UNIQUEIDENTIFIER NOT NULL,
-    quantity INT NOT NULL,
-    created_by_id UNIQUEIDENTIFIER NOT NULL,
-    date_created DATETIME DEFAULT CURRENT_TIMESTAMP,
-    date_modified DATETIME DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (created_by_id) REFERENCES users (id) ON DELETE CASCADE,
-    FOREIGN KEY (parent_item_id) REFERENCES items (id),
-    FOREIGN KEY (child_item_id) REFERENCES items (id)
-);
-CREATE INDEX parent_bill_of_materials_index ON bill_of_materials(parent_item_id);
-CREATE INDEX child_bill_of_materials_index ON bill_of_materials(child_item_id);
-CREATE TABLE order_status (
-    id INT PRIMARY KEY,
-    name NVARCHAR(20) NOT NULL
-);
-CREATE TABLE orders (
-    id UNIQUEIDENTIFIER PRIMARY KEY,
-    customer_id UNIQUEIDENTIFIER NOT NULL,
-    order_date DATETIME NOT NULL,
-    shipping_date DATETIME NOT NULL,
-    notes NVARCHAR(256) NULL,
-    order_status_id INT NOT NULL,
-    date_created DATETIME DEFAULT CURRENT_TIMESTAMP,
-    date_modified DATETIME DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (order_status_id) REFERENCES order_status (id),
-    FOREIGN KEY (customer_id) REFERENCES customer (id) ON DELETE CASCADE
-);
-CREATE TABLE order_items (
-    id UNIQUEIDENTIFIER PRIMARY KEY,
-    order_id UNIQUEIDENTIFIER NOT NULL,
-    item_id UNIQUEIDENTIFIER NOT NULL,
-    quantity INT NOT NULL,
-    price DECIMAL(10, 2) NOT NULL,
-    notes NVARCHAR(256) NULL,
-    date_created DATETIME DEFAULT CURRENT_TIMESTAMP,
-    date_modified DATETIME DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (order_id) REFERENCES orders (id) ON DELETE CASCADE,
-    FOREIGN KEY (item_id) REFERENCES items (id)
-);
-CREATE TABLE inventory (
-    id UNIQUEIDENTIFIER PRIMARY KEY,
-    item_id UNIQUEIDENTIFIER NOT NULL,
-    quantity INT NOT NULL,
-    date_created DATETIME DEFAULT CURRENT_TIMESTAMP,
-    date_modified DATETIME DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (item_id) REFERENCES items (id) ON DELETE CASCADE
-);
+-- create table inventory (
+--     id UNIQUEIDENTIFIER DEFAULT NEWID() PRIMARY KEY,
+--     item_id uniqueidentifier not null,
+--     quantity int not null,
+--     date_created datetime default current_timestamp,
+--     date_modified datetime default current_timestamp,
+--     foreign key (item_id) references items (id) on delete cascade
+-- );
